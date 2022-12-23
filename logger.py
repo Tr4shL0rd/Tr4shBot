729deef15ea200dc42f21c8f4eeb89c2bb01df20
Tr4shL0rd <ketil04@gmail.com>
2022-12-23 17:58:49 +0100

    updated logger

--- logger.py
+++ logger.py
@@ -7,28 +7,37 @@ class Logger:
         self.sys_log_file  = open("logs/syslog.log", "a+", encoding=("utf-8"))
         self.current_time  = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
+    def log_boilerplate(self,message,event:str):
+        return f"[{event.upper()}][{message.created_at}] [{message.author.name}#{message.author.discriminator} -> {message.channel.name}]:"
+
     def close_chat_file(self):
         self.chat_log_file.close()
 
     def close_sys_file(self):
         self.sys_log_file.close()
-    
+
     def close_files(self):
         self.close_chat_file()
         self.close_sys_file()
-        
+
     def chat_log(self, message) -> str:
-        msg = f"[CHAT][{message.created_at}] [{message.author.name}#{message.author.discriminator} -> {message.channel.name}]: {message.content}"
+        msg = f"{self.log_boilerplate(message, 'chat')} {message.content}"
         self.chat_log_file.write(f"{msg}\n") 
         self.chat_log_file.flush()
         return msg
 
     def chat_edit_log(self,message_before,message_after) -> str:
-        msg = f"[EDIT][{message_before.created_at}] [{message_before.author.name}#{message_before.author.discriminator} -> {message_before.channel.name}] {message_before.content} => {message_after.content}"
+        msg = f"[EDIT][{message_before.created_at}] [{message_before.author.name}#{message_before.author.discriminator} -> {message_before.channel.name}]: {message_before.content} => {message_after.content}"
         self.chat_log_file.write(f"{msg}\n")        
         self.chat_log_file.flush()
         return msg
-    
+
+    def chat_delete_log(self,message) -> str:
+        msg = f"{self.log_boilerplate(message, 'delete')} {message.content}"
+        self.chat_log_file.write(f"{msg}\n")
+        self.chat_log_file.flush()
+        return msg
+
     def sys_log(self,message):
         msg = f"[SYS][{self.current_time}]: {message}"
         self.sys_log_file.write(f"{msg}\n")
